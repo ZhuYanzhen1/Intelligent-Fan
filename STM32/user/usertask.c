bd51715c99a4030728a5822d5723b5f96a593463
Lao·Zhu <2208213223@qq.com>
2021-03-17 00:40:21 +0800

    Add temperature control mode

--- STM32/user/usertask.c
+++ STM32/user/usertask.c
@@ -9,7 +9,45 @@ SemaphoreHandle_t GUI_Printf_Semaph;
 void Semaphore_Config() {
     GUI_Printf_Semaph = xSemaphoreCreateMutex();
 }
+void Temp_Process() {
+    if (Menu.Temperature < 2300)
+        Fan_Off();
+    else if (Menu.Temperature >= 2300 && Menu.Temperature <= 2800)
+        Fan_On(20);
+    else
+        Fan_On(40);
+}
+void Auto_Process() {
 
+}
+void Manual_Process() {
+    if (Menu.Heater_Enable == ENABLE)
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
+    else
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
+    if (Menu.Fan_Speed > 0)
+        Fan_On(Menu.Fan_Speed);
+    else if (Menu.Fan_Speed == 0)
+        Fan_Off();
+    if (Menu.Timer.Enable == ENABLE) {
+        if (Menu.Timer.Second == 0) {
+            if (Menu.Timer.Minute == 0) {
+                if (Menu.Timer.Hour == 0) {
+                    Menu.Timer.Enable = DISABLE;
+                    Menu.Fan_Speed = 0;
+                    Fan_Off();
+                } else {
+                    Menu.Timer.Hour--;
+                    Menu.Timer.Minute = 59;
+                }
+            } else {
+                Menu.Timer.Minute--;
+                Menu.Timer.Second = 59;
+            }
+        } else
+            Menu.Timer.Second--;
+    }
+}
 void CircleTask(void *pvParameters) {
     while (1) {
         Menu.Temperature = DS18B20_GetTemp();
@@ -17,31 +55,14 @@ void CircleTask(void *pvParameters) {
         GUI_Printf(30, 8 + Y_Bias, C_BLACK, C_WHITE, "%s", Menu.Time_Buf);
         xSemaphoreGive(GUI_Printf_Semaph);
         Delayms(1000);
-        if (Menu.Heater_Enable == ENABLE)
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
-        else
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
-        if (Menu.Fan_Speed > 0)
-            Fan_On(Menu.Fan_Speed);
-        else if (Menu.Fan_Speed == 0)
-            Fan_Off();
-        if (Menu.Timer.Enable == ENABLE) {
-            if (Menu.Timer.Second == 0) {
-                if (Menu.Timer.Minute == 0) {
-                    if (Menu.Timer.Hour == 0) {
-                        Menu.Timer.Enable = DISABLE;
-                        Menu.Fan_Speed = 0;
-                        Fan_Off();
-                    } else {
-                        Menu.Timer.Hour--;
-                        Menu.Timer.Minute = 59;
-                    }
-                } else {
-                    Menu.Timer.Minute--;
-                    Menu.Timer.Second = 59;
-                }
-            } else
-                Menu.Timer.Second--;
+        switch (Menu.Index) {
+            case 1:Manual_Process();
+                break;
+            case 2:Temp_Process();
+                break;
+            case 3:Auto_Process();
+                break;
+            default:break;
         }
     }
 }
